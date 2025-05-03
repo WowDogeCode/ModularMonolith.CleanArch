4928406e21676c81d214015c7e9246b697f9440f
WowDogeCode <anil.basol34@gmail.com>
2025-05-03 22:37:32 +0300

    Order entity added.

---
+++ Orders.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Orders.Domain.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; }
+        public int? EmployeeId { get; }
+        public int? CustomerId { get; }
+        public int? ShipVia { get; }
+        public DateTime? OrderDate { get; private set; }
+        public DateTime? RequiredDate { get; private set; }
+        public DateTime? ShippedDate { get; private set; }
+        public decimal Freight { get; private set; }
+        public string? ShipName { get; private set; }
+        public string? ShipAddress { get; private set; }
+        public string? ShipCity { get; private set; }
+        public string? ShipRegion { get; private set; }
+        public string? ShipPostalCode { get; private set; }
+        public string? ShipCountry { get; private set; }
+    }
+}
