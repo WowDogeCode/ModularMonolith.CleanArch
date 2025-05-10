309fe1b4ff33ad0ce74019fd84864cc678d7eef5
WowDogeCode <anil.basol34@gmail.com>
2025-05-10 23:30:11 +0300

    IProductRepository and ProductRepository are added.

---
+++ Products.Application/Abstraction/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Products.Domain.Entities;
+
+namespace Products.Application.Abstraction.Repositories
+{
+    public interface IProductRepository
+    {
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int productId);
+    }
+}
---
+++ Products.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Products.Application.Abstraction.Repositories;
+using Products.Domain.Entities;
+
+namespace Products.Infrastructure.Repositories
+{
+    sealed class ProductRepository : IProductRepository
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
