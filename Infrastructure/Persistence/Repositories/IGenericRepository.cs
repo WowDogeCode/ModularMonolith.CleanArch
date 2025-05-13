c262af3305fee1819b4516b343951de0a038974c
WowDogeCode <anil.basol34@gmail.com>
2025-05-13 21:28:35 +0300

    GenericRepository and IGenericRepository are implemented with async support for basic CRUD operations.

---
+++ Infrastructure/Persistence/Repositories/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Persistence.Repositories
+{
+    public interface IGenericRepository<T> where T : class, new()
+    {
+        public Task AddAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task DeleteAsync(int entityId);
+    }
+}
