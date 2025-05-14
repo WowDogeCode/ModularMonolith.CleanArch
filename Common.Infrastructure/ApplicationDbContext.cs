a37e52cc77ac57d182ff4ee6abbefa3889ddddb1
WowDogeCode <anil.basol34@gmail.com>
2025-05-14 15:04:43 +0300

    ApplicationDbContext and GenericRepository are added to Common.Infrastructure.

---
+++ Common.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Domain.Entities;
+
+namespace Common.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Common.Infrastructure/GenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using Common.Application.Abstraction;
+
+namespace Common.Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class, new()
+    {
+        private readonly ApplicationDbContext _context;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int entityId)
+        {
+            T? entityToDelete = await _context.FindAsync<T>(entityId);
+
+            if (entityToDelete != null)
+            {
+                _context.Remove(entityToDelete);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
