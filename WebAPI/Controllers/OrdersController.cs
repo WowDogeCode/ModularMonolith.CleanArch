647c1372401d98a662a89a514847823c35310396
WowDogeCode <anil.basol34@gmail.com>
2025-07-29 20:23:00 +0300

    OrdersController added.

---
+++ WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Orders.Application.Orders.AddOrder;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("add-order")]
+        public async Task<ActionResult<int>> AddOrder([FromBody] AddOrderCommand addOrder)
+        {
+            int result = await _mediator.Send(addOrder).ConfigureAwait(false);
+            return Ok(result);
+        }
+    }
+}
