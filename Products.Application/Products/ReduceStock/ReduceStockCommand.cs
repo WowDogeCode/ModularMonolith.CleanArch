03315a26f0a317434818709024efdc78f2d48172
WowDogeCode <anil.basol34@gmail.com>
2025-08-27 20:44:17 +0300

    ReduceStockCommand and ReduceStockCommandHandler are implemented.

---
+++ Products.Application/Products/ReduceStock/ReduceStockCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Products.Application.Products.ReduceStock
+{
+    public record ReduceStockCommand : IRequest<bool>
+    {
+        public int ProductId { get; init; }
+        public int Quantity { get; init; }
+    }
+}
---
+++ Products.Application/Products/ReduceStock/ReduceStockCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Common.Application.Abstraction;
+using MediatR;
+using Products.Application.Abstraction.Repositories;
+using Products.Domain.Entities;
+
+namespace Products.Application.Products.ReduceStock
+{
+    public sealed class ReduceStockCommandHandler : IRequestHandler<ReduceStockCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public ReduceStockCommandHandler(IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Handle(ReduceStockCommand request, CancellationToken cancellationToken)
+        {
+            Product? product = await _productRepository.GetByIdAsync(request.ProductId).ConfigureAwait(false);
+
+            if (product != null)
+            {
+                product.DecreaseStock(request.Quantity);
+
+                await _unitOfWork.CommitAsync(cancellationToken);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
