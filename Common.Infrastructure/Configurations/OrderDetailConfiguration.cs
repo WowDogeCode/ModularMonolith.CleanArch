5076ea839a0a9c4044fd1d1c54ef655c72741ea0
WowDogeCode <anil.basol34@gmail.com>
2025-09-08 17:05:27 +0300

    Fixed wrong table name, missing navigation property of Order and added details of money units.

--- Common.Infrastructure/Configurations/OrderDetailConfiguration.cs
+++ Common.Infrastructure/Configurations/OrderDetailConfiguration.cs
@@ -1,20 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Orders.Domain.Entities;
+using Products.Domain.Entities;
 
-namespace Common.Infrastructure.Configurations
+public sealed class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
 {
-    public sealed class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+    public void Configure(EntityTypeBuilder<OrderDetail> builder)
     {
-        public void Configure(EntityTypeBuilder<OrderDetail> builder)
-        {
-            builder.ToTable("Order_Details");
+        builder.ToTable("Order Details");
 
-            builder.HasKey(od => new { od.OrderId, od.ProductId });
+        builder.HasKey(od => new { od.OrderId, od.ProductId });
 
-            builder.HasOne<Order>()
-                .WithMany(o => o.OrderDetails)
-                .HasForeignKey(od => od.OrderId);
-        }
+        builder.HasOne(od => od.Order)
+               .WithMany(o => o.OrderDetails)
+               .HasForeignKey(od => od.OrderId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Product>()
+               .WithMany()
+               .HasForeignKey(od => od.ProductId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(od => od.UnitPrice)
+               .HasColumnType("decimal(18,2)");
+
+        builder.Property(od => od.Discount)
+               .HasColumnType("decimal(18,2)");
+
+        builder.Property(od => od.OrderId)
+               .HasColumnName("OrderID");
+
+        builder.Property(od => od.ProductId)
+               .HasColumnName("ProductID");
+
+        builder.Property(od => od.Quantity)
+               .HasColumnName("Quantity")
+               .HasColumnType("smallint");
     }
 }
