fafa636b5a1d0de8bd15531cc365bbea7309e512
WowDogeCode <anil.basol34@gmail.com>
2026-04-22 21:53:34 +0300

    PlaceOrderCommandHandler refactored to work with a transaction scope, stock check validations and stock reduction are refactored.

--- Orders.Application/Orders/PlaceOrder/PlaceOrderCommandHandler.cs
+++ Orders.Application/Orders/PlaceOrder/PlaceOrderCommandHandler.cs
@@ -26,35 +26,73 @@ namespace Orders.Application.Orders.PlaceOrder
             var validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
             if (!validationResult.IsValid)
-            {
                 throw new ValidationException(validationResult.Errors);
-            }
+
+            await _unitOfWork.BeginTransactionAsync(cancellationToken);
 
             var orderDetails = request.OrderDetails.Select(dto => new OrderDetail(dto.ProductId, dto.UnitPrice, dto.Quantity, dto.Discount)).ToList();
 
-            var orderToAdd = Order.Create(request.EmployeeId, request.CustomerId, request.ShipVia,
+            var productsInventoryInfo = await _inventoryService.GetProductInventorySnapshotsAsync(orderDetails.Select(od => od.ProductId).ToList(), cancellationToken);
+
+            if (productsInventoryInfo.Any(x => x.Discontinued == true))
+                throw new ValidationException("One or more products are discontinued and cannot be ordered");
+
+            if (orderDetails.Select(od => od.ProductId).Except(productsInventoryInfo.Select(x => x.ProductId)).Any())
+                throw new ValidationException("One or more products in the order details do not exist in inventory");
+
+            var orderDetailsDict = orderDetails.ToDictionary(od => od.ProductId, od => od);
+
+            try
+            {
+                var orderToAdd = Order.Create(request.EmployeeId, request.CustomerId, request.ShipVia,
                 request.RequiredDate, null, request.Freight, request.ShipName, request.ShipAddress,
                 request.ShipCity, request.ShipRegion, request.ShipPostalCode, request.ShipCountry,
                 orderDetails);
 
-            foreach (var item in orderToAdd.OrderDetails)
-            {
-                await _inventoryService.ReduceStockAsync(item.ProductId, item.Quantity, cancellationToken);
+                await _orderRepository.AddAsync(orderToAdd);
+
+                foreach (var item in productsInventoryInfo)
+                {
+                    if (!orderDetailsDict.TryGetValue(item.ProductId, out var orderDetail))
+                        continue;
+
+                    bool result;
+
+                    if (item.UnitsInStock < orderDetail.Quantity)
+                    {
+                        if (item.UnitsInStock + item.UnitsOnOrder < orderDetail.Quantity)
+                            throw new ValidationException($"Not enough stock for product {item.ProductName}. Available: {item.UnitsInStock}, On Order: {item.UnitsOnOrder}, Required: {orderDetail.Quantity}");
+
+                        result = await _inventoryService.ReduceStockAsync(item.ProductId, (short)item.UnitsInStock, cancellationToken).ConfigureAwait(false);
+
+                        if (result == false)
+                            throw new ValidationException($"Failed to reserve available stock for product {item.ProductName}. Please try again.");
+
+                        continue;
+                    }
+
+                    result = await _inventoryService.ReduceStockAsync(item.ProductId, (short)(orderDetail.Quantity), cancellationToken).ConfigureAwait(false);
+
+                    if (!result)
+                        throw new ValidationException($"Failed to reserve stock for product {item.ProductName}. Please try again.");
+                }
+
+                await _unitOfWork.CommitAsync(cancellationToken);
+
+                return new PlaceOrderResponseDto
+                {
+                    OrderId = orderToAdd.Id,
+                    CustomerId = orderToAdd.CustomerId,
+                    OrderDate = orderToAdd.OrderDate,
+                    TotalAmount = orderToAdd.OrderDetails.Sum(x => x.Quantity * x.UnitPrice),
+                    ItemsCount = orderToAdd.OrderDetails.Count()
+                };
             }
-
-            await _orderRepository.AddAsync(orderToAdd);
-            await _unitOfWork.CommitAsync(cancellationToken);
-
-            var response = new PlaceOrderResponseDto
+            catch (Exception)
             {
-                OrderId = orderToAdd.Id,
-                CustomerId = orderToAdd.CustomerId,
-                OrderDate = orderToAdd.OrderDate,
-                TotalAmount = orderToAdd.OrderDetails.Sum(x => x.Quantity * x.UnitPrice),
-                ItemsCount = orderToAdd.OrderDetails.Count()
-            };
-
-            return response;
+                await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                throw;
+            }
         }
     }
 }
