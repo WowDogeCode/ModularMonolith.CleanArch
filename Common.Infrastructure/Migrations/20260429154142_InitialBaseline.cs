6a02a4e7ca66da75307e527b5e97e2bc476772db
WowDogeCode <anil.basol34@gmail.com>
2026-04-29 18:52:44 +0300

    EF Migration files for adding OrderStatus column to Order table.

---
+++ Common.Infrastructure/Migrations/20260429154142_InitialBaseline.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Common.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialBaseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderStatus",
+                table: "Orders",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "Pending");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderStatus",
+                table: "Orders");
+        }
+    }
+}
